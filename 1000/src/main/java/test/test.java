74b31a3a08d3b6bb451aabd2c8f988992c8b56da
Parag Jadhav <paragjadhav@gmail.com>
2022-03-20 07:47:55 +0530

    First commit
    All commit files

---
+++ 1000/src/main/java/test/test.java
@@ -0,0 +1,18 @@
+
+package test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.setProperty("webdriver.chrome.driver","D:\\ParagData\\Devops_Paid_Training\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.gmail.com/");
+		System.out.println("test successful");
+	}
+
+}
